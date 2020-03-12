633741673c60ecfebd53be80851858e9f29dea96
Serwio YK <kucelas@ukr.net>
2020-03-12 23:26:26 +0200

    Remove lib

--- searcher/src/main/java/parser.java
+++ searcher/src/main/java/parser.java
@@ -1,16 +1,5 @@
-import java.io.IOException;
-import java.net.URL;
 
-import org.jsoup.nodes.Document;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-
-public class parser extends JFrame {
+public class parser extends {
 
 
     public static void main(String[] args) throws IOException {
